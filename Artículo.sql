bdb56a4ab5a8dbabbcdc02929221cee56a52483e
sayaann <32908097+sayaann@users.noreply.github.com>
2017-12-10 00:47:37 -0600

    Add files via upload

---
+++ Artículo.sql
@@ -0,0 +1,14 @@
+/*
+-- Query: 
+-- Date: 2017-12-10 00:34
+*/
+INSERT INTO `Artículo` (`idArtículo`,`Nombre`,`Descripción`,`Precio`,`Unidades`,`Imagen`) VALUES (150,'Resistencia 1 K','Resistencia 1 K 5 resistencias',NULL,100,NULL);
+INSERT INTO `Artículo` (`idArtículo`,`Nombre`,`Descripción`,`Precio`,`Unidades`,`Imagen`) VALUES (644,'Pinzas de corte','Pinzas de corte diagonal',70.00,50,NULL);
+INSERT INTO `Artículo` (`idArtículo`,`Nombre`,`Descripción`,`Precio`,`Unidades`,`Imagen`) VALUES (122,'Multímetro digital','Multímetro digital con luz de fondo',215.00,40,NULL);
+INSERT INTO `Artículo` (`idArtículo`,`Nombre`,`Descripción`,`Precio`,`Unidades`,`Imagen`) VALUES (456,'Diodo zener','Paquete de 10 Diodo zener',20.00,200,NULL);
+INSERT INTO `Artículo` (`idArtículo`,`Nombre`,`Descripción`,`Precio`,`Unidades`,`Imagen`) VALUES (546,'Protoboard','Protoboar 400 ountos',40.00,100,NULL);
+INSERT INTO `Artículo` (`idArtículo`,`Nombre`,`Descripción`,`Precio`,`Unidades`,`Imagen`) VALUES (754,'Transistor PNP','Transistor de pequeña señal tipo PNP',5.00,300,NULL);
+INSERT INTO `Artículo` (`idArtículo`,`Nombre`,`Descripción`,`Precio`,`Unidades`,`Imagen`) VALUES (755,'Transistor NPN','Transistor de pequeña señal NPN',4.00,250,NULL);
+INSERT INTO `Artículo` (`idArtículo`,`Nombre`,`Descripción`,`Precio`,`Unidades`,`Imagen`) VALUES (213,'Cable estañado','Rollo de cable estañado 100 metros',195.00,40,NULL);
+INSERT INTO `Artículo` (`idArtículo`,`Nombre`,`Descripción`,`Precio`,`Unidades`,`Imagen`) VALUES (489,'Capacitor 100 microF','Capacitorelectrolítico 100 microF',4.00,100,NULL);
+INSERT INTO `Artículo` (`idArtículo`,`Nombre`,`Descripción`,`Precio`,`Unidades`,`Imagen`) VALUES (741,'Capacitor 0.001 microF','Capacitor de 0.001 microF',3.00,200,NULL);
